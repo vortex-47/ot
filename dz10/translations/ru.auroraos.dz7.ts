8e576833af78af892cd8d71d1e091aee511ab247
vortex-47 <vortex-47@mail.ru>
2023-10-03 18:07:29 +0700

    Home work #10

---
+++ dz10/translations/ru.auroraos.dz7.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="46"/>
+        <source>Notes</source>
+        <translation>Notes</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <source>Notes</source>
+        <translation>Notes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="57"/>
+        <source>Edit note</source>
+        <translation>Edit note</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="66"/>
+        <source>Delete note</source>
+        <translation>Delete note</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="83"/>
+        <source>Add note</source>
+        <translation>Add note</translation>
+    </message>
+</context>
+<context>
+    <name>NotePage</name>
+    <message>
+        <location filename="../qml/pages/NotePage.qml" line="56"/>
+        <source>Save</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotePage.qml" line="57"/>
+        <source>Cancel</source>
+        <translation>Cancel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotePage.qml" line="62"/>
+        <source>Enter date</source>
+        <translation>Enter date</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotePage.qml" line="63"/>
+        <source>Date note</source>
+        <translation>Date note</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotePage.qml" line="71"/>
+        <source>Enter note</source>
+        <translation>Enter note</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/NotePage.qml" line="72"/>
+        <source>Text note</source>
+        <translation>Text note</translation>
+    </message>
+</context>
+</TS>
